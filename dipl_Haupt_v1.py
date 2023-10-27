fa8ec848a002796c92558bea2f8195fd1459d387
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2023-10-27 12:55:52 +0200

    Einfuehrung in MainWindow

--- dipl_Haupt_v1.py
+++ dipl_Haupt_v1.py
@@ -1,8 +1,11 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
 
-class Frm_main(QMainWindow):
+from dipl_Einfuehrung.einfuehrung_v1 import Ui_MainWindow
+
+class Frm_main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
 
 app = QApplication()
 frm_main = Frm_main()
