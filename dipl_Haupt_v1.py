37b169db66986741cd43fe48f5aafc267e960ad8
diplomarbeit-PCR <149083138+diplomarbeit-PCR@users.noreply.github.com>
2023-10-27 11:28:51 +0200

    Add files via upload

---
+++ dipl_Haupt_v1.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+class Frm_main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+app = QApplication()
+frm_main = Frm_main()
+frm_main.show()
+app.exec()
