b7f8dd6de805b810f44374674a2dfd2b4ab828d9
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2023-11-22 14:31:30 +0100

    Denaturierungsfenster ersetzt Startfenster

--- dipl_Haupt_v1.py
+++ dipl_Haupt_v1.py
@@ -3,12 +3,25 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 from dipl_Einfuehrung.einfuehrung_v3 import Ui_StartWindow
 from dipl_Ablauf_Denat.AblaufWindowDenat_v1 import Ui_AblaufWindowDenat
 
-class Frm_main(QMainWindow, Ui_MainWindow):
+class Frm_denat(QMainWindow, Ui_AblaufWindowDenat):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+class Frm_main(QMainWindow, Ui_StartWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.btn_Start.clicked.connect(self.denat_Ablauf)
+
+    def denat_Ablauf(self):
+        frm_main.hide()
+        frm_denat.show()
+
+
 app = QApplication()
 frm_main = Frm_main()
 frm_main.show()
+frm_denat = Frm_denat()
 app.exec()
