ac66b9dd372a65d997c3776abbe69eeeabab3ec4
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2023-11-27 15:03:30 +0100

    Durchlaufzähler hinzugefügt

--- dipl_Haupt_v1.py
+++ dipl_Haupt_v1.py
@@ -1,7 +1,7 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QLCDNumber
 from PySide6.QtCore import QTimer, QTimer
 
-from dipl_Einfuehrung.einfuehrung_v3 import Ui_StartWindow
+from dipl_Einfuehrung.einfuehrung_v4 import Ui_StartWindow
 from dipl_Phasenablauf.AblaufWindowDenat_v1 import Ui_AblaufWindowDenat
 from dipl_Phasenablauf.AblaufWindowAneal_v1 import Ui_AblaufWindowAneal
 from dipl_Phasenablauf.AblaufWindowSens_v1 import Ui_AblaufWindowSens
@@ -14,25 +14,40 @@ class Frm_denat(QMainWindow, Ui_AblaufWindowDenat):
         super().__init__()
         self.setupUi(self)
 
+    def update_DL_zaehler(self, value):
+        self.DL_zaehler.display(value)
+
 class Frm_aneal(QMainWindow, Ui_AblaufWindowAneal):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+    def update_DL_zaehler(self, value):
+        self.DL_zaehler.display(value)
 
 class Frm_sens(QMainWindow, Ui_AblaufWindowSens):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+    def update_DL_zaehler(self, value):
+        self.DL_zaehler.display(value)
+
 class Frm_asens(QMainWindow, Ui_AblaufWindowASens):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+    def update_DL_zaehler(self, value):
+        self.DL_zaehler.display(value)
+
 class Frm_elong(QMainWindow, Ui_AblaufWindowElong):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+    def update_DL_zaehler(self, value):
+        self.DL_zaehler.display(value)
 
 class Frm_kont(QMainWindow, Ui_Kontrolle):
     def __init__(self):
@@ -44,6 +59,8 @@ class Frm_main(QMainWindow, Ui_StartWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.DL_zaehler_value = 0
 
         self.frm_denat = Frm_denat()
         self.frm_aneal = Frm_aneal()
@@ -75,7 +92,13 @@ class Frm_main(QMainWindow, Ui_StartWindow):
             self.run_phasen_Ablauf()
     
     def run_phasen_Ablauf(self):
-        #self.phasen_running = True  # Starte phasen_Ablauf
+        self.DL_zaehler_value += 1  
+        self.frm_denat.update_DL_zaehler(self.DL_zaehler_value)
+        self.frm_aneal.update_DL_zaehler(self.DL_zaehler_value)
+        self.frm_sens.update_DL_zaehler(self.DL_zaehler_value)
+        self.frm_asens.update_DL_zaehler(self.DL_zaehler_value)
+        self.frm_elong.update_DL_zaehler(self.DL_zaehler_value)
+
         self.frm_denat.show()
         QTimer.singleShot(1000, self.frm_denat.hide)
         QTimer.singleShot(1000, self.frm_aneal.show)
@@ -93,13 +116,16 @@ class Frm_main(QMainWindow, Ui_StartWindow):
         self.phasen_running = False  # Stoppe phasen_Ablauf
 
     def weiter(self):
-        self.phasen_running = True
+        self.phasen_running = True   # Starte phasen_Ablauf
+        self.frm_kont.hide()
         self.phasen_Ablauf()
 
     def esc(self):
         self.frm_kont.hide()
+        self.DL_zaehler_value = 0
         frm_main.show()
-
+        self.phasen_running = True
+        self.btn_Start.clicked.connect(self.phasen_Ablauf)
 
 app = QApplication()
 frm_main = Frm_main()
