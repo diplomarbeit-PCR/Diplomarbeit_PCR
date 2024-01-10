a509bd30c42c893e6a44e9f90db67e7f29a363ed
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-01-10 15:41:45 +0100

    neuer i2c code

--- test.py
+++ test.py
@@ -1,34 +1,25 @@
 import smbus
 import time
+bus = smbus.SMBus(2)
 
-# Festlegen des I2C-Bus (normalerweise 1 für Raspberry Pi)
-i2c_bus = 1
+address = 0x27
 
-# Festlegen der I2C-Adresse des Slaves
-i2c_address = 0x04
+def writeNumber(value):
+    bus.write_byte(address, value)
+    return -1
 
-# Erstellen einer I2C-Verbindung
-bus = smbus.SMBus(i2c_bus)
+def readNumber():
+    number = bus.read_byte(address)
+    return number
 
-try:
-    while True:
-        # Hier können Sie Daten an den Slave senden
-        data_to_send = [0x01, 0x02, 0x03]
-        bus.write_i2c_block_data(i2c_address, 0x00, data_to_send)
+while True:
+    inp = input("Number between 1 and 9: ")
+    if not inp:
+        continue
 
-        # Warten Sie einen Moment (optional)
-        time.sleep(1)
+    writeNumber(inp)
+    print ("RPi sends: ", inp)
+    time.sleep(1)
 
-        # Daten vom Slave lesen (nehmen Sie die Anzahl der Bytes, die Sie erwarten)
-        data_received = bus.read_i2c_block_data(i2c_address, 0x00, 3)
-        print("Received data:", data_received)
-
-        # Warten Sie einen Moment (optional)
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    # Schließen Sie die I2C-Verbindung, wenn das Programm beendet wird
-    bus.close()
+    recv = readNumber()
+    print ("Arduino sends: ", recv)
