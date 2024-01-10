b9c84117039d6eedba9a7953f7469c1966af22d6
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-01-10 13:36:39 +0100

    i2c Code von Raspi vorlage

--- test.py
+++ test.py
@@ -2,11 +2,15 @@ import smbus
 import time
 bus = smbus.SMBus(1)
 
-address = 0x04
+address = 0x05
 
 def writeNumber(value):
     bus.write_byte(address, value)
     return -1
+
+def readNumber():
+    number = bus.read_byte(address)
+    return number
 
 while True:
     inp = input("Number between 1 and 9: ")
@@ -14,5 +18,8 @@ while True:
         continue
 
     writeNumber(inp)
-    print ("Rock sends: "), inp
+    print ("RPi sends: ", inp)
     time.sleep(1)
+
+    recv = readNumber()
+    print ("Arduino sends: ", recv)
