0580a9ccfcfaf7ae6594e664b8d9dbf66e066514
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-01-18 08:26:35 +0100

    neuer i2c code

--- test.py
+++ test.py
@@ -1,30 +1,17 @@
 import smbus
 import time
 
-bus_number = 7  # Du kannst auch Bus 0 ausprobieren
+bus = smbus.SMBus(2)
+
 address = 0x27
-#https://prod.liveshare.vsengsaas.visualstudio.com/join?D782CC7934A2ABFE225732FC1764658FBB54
-try:
-    bus = smbus.SMBus(bus_number)
-except FileNotFoundError:
-    print(f"Error: I2C bus {bus_number} not found. Check your connections and bus number.")
-    exit()
 
 def writeNumber(value):
-    try:
-        bus.write_byte(address, value)
-        return None
-    except OSError as e:
-        print(f"Error writing to I2C device: {e}")
-        return e
+    bus.write_byte(address, value)
+    return -1
 
 def readNumber():
-    try:
-        number = bus.read_byte(address)
-        return number
-    except OSError as e:
-        print(f"Error reading from I2C device: {e}")
-        return None
+    number = bus.read_byte(address)
+    return number
 
 while True:
     inp = input("Number between 1 and 9: ")
@@ -32,16 +19,9 @@ while True:
     if not inp:
         continue
 
-    write_result = writeNumber(inp)
-    if write_result is not None:
-        # Handle error, for example, you may choose to break the loop or take corrective action.
-        break
-
-    print("RPi sends:", inp)
+    writeNumber(inp)
+    print ("RPi sends: ", inp)
     time.sleep(1)
 
     recv = readNumber()
-    if recv is not None:
-        # Handle error, for example, you may choose to break the loop or take corrective action.
-        break
-    print("Arduino sends:", recv)
+    print ("Arduino sends: ", recv)
