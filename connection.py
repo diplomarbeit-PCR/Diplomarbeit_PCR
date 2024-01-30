40c7cfc045239072b3f91f4a854086778ec31f3e
Diplom Arbeit <diplomarbeit.pcr@gmail.com>
2024-01-30 14:59:53 +0100

    pymysql

--- connection.py
+++ connection.py
@@ -1,15 +1,21 @@
-import mysql.connector # neue Bibliothek
+import pymysql
 
 Servername = 'localhost'
 Benutzer = 'root'
 Passwort = 'Rock4C+'
-Datenbank = 'eduPCR'
 
 # Verbindung mit der Datenbank
-con = mysql.connector.connect(
+con = pymysql.connect(
     host = Servername,
     user = Benutzer,
     Password = Passwort
 )
-con.database = Datenbank
+cursor = con.cursor()
+cursor.execute("CREATE DATABASE EDUPCR")
+cursor.execute("SHOW DATABASES")
+dataLbaseist = cursor.fetchall()
 
+for database in databaseList:
+    print(database)
+
+con.close()
