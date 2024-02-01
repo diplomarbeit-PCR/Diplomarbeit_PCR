3bd2d38a6cf0064dfc859816075c72fb261d77f7
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-02-01 08:35:40 +0100

    Daten in richtige tbl ausgeben

--- dipl_Haupt_v2.py
+++ dipl_Haupt_v2.py
@@ -296,16 +296,14 @@ class Frm_main(QMainWindow, Ui_StartWindow):
 
     def ergebnis(self):
         # Tabellenwidget für PhasenWerte erstellen
-        self.tbl_phasen = QTableWidget()
-        self.tbl_phasen.setColumnCount(5)  # Fünf Spalten
-        self.tbl_phasen.setHorizontalHeaderLabels(["Kategorien", "Denaturierung", "Annealing", "Elongation", "Einheit"])
-        self.setCentralWidget(self.tbl_phasen)
+        self.frm_ergeb.tbl_phasen = QTableWidget()
+        self.frm_ergeb.tbl_phasen.setColumnCount(5)  # Fünf Spalten
+        self.setCentralWidget(self.frm_ergeb.tbl_phasen)
 
         # Tabellenwidget für Messwerte erstellen
-        self.tbl_messwerte = QTableWidget()
-        self.tbl_messwerte.setColumnCount(2)  # Drei Spalten
-        self.tbl_messwerte.setHorizontalHeaderLabels(["Kategorien", "Anzahl"])
-        self.setCentralWidget(self.tbl_messwerte)
+        self.frm_ergeb.tbl_mess = QTableWidget()
+        self.frm_ergeb.tbl_mess.setColumnCount(2)  # Drei Spalten
+        self.setCentralWidget(self.frm_ergeb.tbl_mess)
 
         try:
             # Tabelle 'PhasenWerte' erstellen, falls nicht vorhanden
@@ -359,15 +357,15 @@ class Frm_main(QMainWindow, Ui_StartWindow):
 
             # Ergebnisse in tbl_phasen einfügen
             for row_num, row_data in enumerate(result_phasen):
-                self.tbl_phasen.insertRow(row_num)
+                self.frm_ergeb.tbl_phasen.insertRow(row_num)
                 for col_num, col_data in enumerate(row_data):
-                    self.tbl_phasen.setItem(row_num, col_num, QTableWidgetItem(str(col_data)))
+                    self.frm_ergeb.tbl_phasen.setItem(row_num, col_num, QTableWidgetItem(str(col_data)))
 
             # Ergebnisse in tbl_messwerte einfügen
             for row_num, row_data in enumerate(result_messwerte):
-                self.tbl_messwerte.insertRow(row_num)
+                self.frm_ergeb.tbl_mess.insertRow(row_num)
                 for col_num, col_data in enumerate(row_data):
-                    self.tbl_messwerte.setItem(row_num, col_num, QTableWidgetItem(str(col_data)))
+                    self.frm_ergeb.tbl_mess.setItem(row_num, col_num, QTableWidgetItem(str(col_data)))
 
 
         except pymysql.MySQLError as e:
