a82f6aa2fa18b7992a2f97393e0df3bbc7f90cbd
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-02-13 13:59:26 +0100

    neu

--- dipl_I2C/i2c_connection_beweg.py
+++ dipl_I2C/i2c_connection_beweg.py
@@ -4,7 +4,7 @@ import time
 # Verwenden von I2C Bus 7
 bus = smbus.SMBus(7)
 
-beweg_address = 0x28
+beweg_address = 0x028
 
 value_denat = 10
 value_aneal_gesamt = 30
@@ -17,14 +17,27 @@ def writeNumber(value):
     return -1
 
 while True:
-    inp =  [value_denat, value_aneal_gesamt, value_elong_gesamt]
+    inp = input("Number between 1 and 3: ")
     inp = int(inp)
     if not inp:
         continue
 
-    writeNumber(inp)
-    print ("Rock sends: ", inp)
-    inp+1
+    if inp == 1:
+        value = value_denat
+
+        
+    if inp == 2:
+        value = value_aneal_gesamt
+
+        
+    if inp == 3:
+        value = value_elong_gesamt
+
+    else:
+        value = 0 
+
+    writeNumber(value)
+    print ("RPi sends: ", value)
     time.sleep(1)
 
    
