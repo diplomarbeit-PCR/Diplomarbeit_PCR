832e7d554add68187a77a0beea009ddfa0d5986e
Diplom Arbeit <diplomarbeit.pcr@gmail.com>
2024-02-14 14:44:41 +0100

    beweg funktioniert

--- dipl_I2C/i2c_connection_beweg_recvreq.py
+++ dipl_I2C/i2c_connection_beweg_recvreq.py
@@ -9,6 +9,7 @@ beweg_address = 0x28
 value_denat = 10
 value_aneal_gesamt = 30
 value_elong_gesamt = 20
+b = 0
 
 # Kommunikation mit Bewegmechanismus
 
@@ -22,44 +23,40 @@ def readFromBeweg():
 
         # fragt ab, ob in 0-Posi
         if b == 5:
-            beweg_null = bus.read_byte(beweg_address)
-        
+            #beweg_null = bus.read_byte(beweg_address)
+            print("Nullposi")
+            writeNumber(4)
+
         # falls Notaus gedrückt -> Stop everything
         if b == 9:
-            notaus = bus.read_byte(beweg_address)
-
-        else:
-            print(f"No valid input")
-
-        return notaus, beweg_null
- 
+            #notaus = bus.read_byte(beweg_address)
+            print("Notaus")
+        
     except OSError as e:
         print(f"Error reading from I2C device: {e}")
         return None
-
+    
 while True:
+    b = readFromBeweg()
+    if b == 0:
+        continue
+
     inp = input("Number between 1 and 3: ")
     inp = int(inp)
     if not inp:
         continue
 
     if inp == 1:
-        #writeNumber(inp)
         value = value_denat
         value = int(value)
         writeNumber(inp)
         print ("RPi sends: ", inp)
 
-
-        
     if inp == 2:
-        #writeNumber(inp)
         value = value_aneal_gesamt
         value = int(value)
         writeNumber(inp)
         print ("RPi sends: ", inp)
-
-
         
     if inp == 3:
         value = value_elong_gesamt
@@ -67,10 +64,9 @@ while True:
         writeNumber(inp)
         print ("RPi sends: ", inp)
 
-    else:
-        value = int(inp)
+    if inp != 1 and inp != 2 and inp !=3:
+        value = inp
 
-    time.sleep(2)
     writeNumber(value)
     print ("RPi sends: ", value)
     time.sleep(1)
