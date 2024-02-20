1ec7e50682d70e04d0d8e8e4b69dc6ad8febdfea
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-02-20 15:22:07 +0100

    sdd

--- dipl_I2C/i2c_connection_detect.py
+++ dipl_I2C/i2c_connection_detect.py
@@ -27,7 +27,28 @@ def readFromDetect():
     except OSError as e:
         print(f"Error reading from I2C device: {e}")
         return None
+    
+
+def readFromDetector():
+    try:
+        # Lesen des zweiten Messwerts (Licht)
+        light = bus.read_byte(detect_address)
+        print("Empfangener Licht-Wert:", light)
+
+        # Lesen des ersten Messwerts (SPG)
+        spg = bus.read_byte(detect_address)
+        print("Empfangener SPG-Wert:", spg)
+
+        return spg, light
+
+    except OSError as e:
+        print(f"Fehler beim Lesen vom I2C-Gerät: {e}")
+        return None, None
 
 d = readFromDetect()
+while True:
+    spg, light = readFromDetector()
+    time.sleep(1)  # Führt die Messung alle Sekunde erneut durch
+
 
     
