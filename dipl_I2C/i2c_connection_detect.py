6e86e8bb54cde69cb94e322bee213b06fe848959
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-02-20 13:40:50 +0100

    jlbdcs

--- dipl_I2C/i2c_connection_detect.py
+++ dipl_I2C/i2c_connection_detect.py
@@ -1,16 +1,13 @@
 import smbus
 import time
 
-
 # Verwenden von I2C Bus 7
 bus = smbus.SMBus(7)
-# Deklarieren der Adressen der Slaves
-
+# Deklarieren der Adresse des Slaves
 detect_address = 0x27
 
 value_spg = 0
 value_light = 0
-d = 0
 d_alt = 0
 
 # Kommunikation mit Detektor
@@ -19,52 +16,31 @@ def readFromDetect():
         print("in Lesefkt")
         d = bus.read_byte(detect_address)
         print("gelesen: ", d)
-        
 
-        # fragt ab, ob in 0-Posi
-        if d == 5:
-            print("Nullposi")
-            
-
-        # falls Notaus gedrückt -> Stop everything
-        if d == 9:
-            print("Notaus")
+        return d
         
     except OSError as e:
         print(f"Error reading from I2C device: {e}")
         return None
 
-    return(d)
-
 while True:
-
-
-    if d != 5:
-        d = readFromDetect()  
-
-    if d == 7:
-       print("Warten")
-       time.sleep(2)
-       d = readFromDetect()
+    d = readFromDetect()
 
     if d == 5:
-        
-
         if d_alt == 1:
-            value = readFromDetect()
-            value_spg = int(value)
-            print ("RPi sends: ", value_spg)
+            value_spg = bus.read_byte(detect_address)
+            print("RPi sends spg:", value_spg)
             d_alt = 0
 
         if d_alt == 2:
-            value = readFromDetect
-            value_light = int(value)
-            print ("RPi sends: ", value_light)
+            value_light = bus.read_byte(detect_address)
+            print("RPi sends light:", value_light)
             d_alt = 0
 
-        d_alt = d
-        
+    elif d == 7:
+        print("Warten")
+        time.sleep(2)
+    elif d == 9:
+        print("Notaus")
 
-
-
-   
+    d_alt = d
