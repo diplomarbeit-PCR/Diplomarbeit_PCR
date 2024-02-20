73f6a498f7a72a380d4d4952ed2c21079a76b190
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-02-20 13:16:42 +0100

    test

--- dipl_I2C/i2c_connection_detect.py
+++ dipl_I2C/i2c_connection_detect.py
@@ -8,39 +8,66 @@ bus = smbus.SMBus(7)
 
 detect_address = 0x27
 
-
-
 # Kommunikation mit Detektor
 def readFromDetekt():
     try:
         # Daten vom Slave anfordern
-        bus.request_from(detect_address)
-        if bus.available():
-            d = bus.read()
-            if d == 1:
-                # Nachricht erhalten, dass der Messwert 1 gesendet wird
-                print("Nachricht erhalten: Messwert 1 wird gesendet")
-                # Messwert 1 anfordern
-                bus.request_from(detect_address)
-                if bus.available():
-                    value_spg = bus.read()
-                    print("Messwert 1:", value_spg)
-            elif d == 2:
-                # Nachricht erhalten, dass der Messwert 2 gesendet wird
-                print("Nachricht erhalten: Messwert 2 wird gesendet")
-                # Messwert 2 anfordern
-                bus.request_from(detect_address)
-                if bus.available():
-                    value_light = bus.read()
-                    print("Messwert 2:", value_light)
-            else:
-                # Nachricht erhalten, dass die Bedingung nicht erfüllt ist
-                print("Bedingung nicht erfüllt")
+        print("in Lesefkt")
+        d = bus.read_byte(detect_address)        
+        print("gelesen: ", d)
 
-            print(value_spg)    
-            print(value_light) 
+        # fragt ab, ob in 0-Posi
+        if d == 5:
+            #beweg_null = bus.read_byte(beweg_address)
+            print("Nullposi")
+            
+
+        # falls Notaus gedrückt -> Stop everything
+        if d == 9:
+            #notaus = bus.read_byte(beweg_address)
+            print("Notaus")
+
+        else:
+            # Nachricht erhalten, dass die Bedingung nicht erfüllt ist
+            print("Bedingung nicht erfüllt")
+
+        print(value_spg)    
+        print(value_light) 
         return value_spg, value_light
  
     except OSError as e:
         print(f"Error reading from I2C device: {e}")
         return None
+
+
+while True:
+
+
+    if d != 5:
+        d = readFromDetekt()  
+
+    if d == 7:
+       print("Warten")
+       time.sleep(2)
+       d = readFromDetekt()
+
+    if d == 5:
+        
+        w = bus.read_byte(detect_address)    
+
+        if w_alt == 1:
+            value = bus.read_byte(detect_address)
+            value_spg = int(value)
+            print ("RPi sends: ", value_spg)
+            w_alt = 0
+
+        if w_alt == 2:
+            value = bus.read_byte(detect_address)
+            value_light = int(value)
+            print ("RPi sends: ", value_light)
+            w_alt = 0
+        
+        w_alt = w
+
+
+   
