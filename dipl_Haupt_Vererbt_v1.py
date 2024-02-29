62ec19ee96814bb3e46bb25f5c972335f11a5a4f
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-02-29 18:21:55 +0100

    cs

--- dipl_Haupt_Vererbt_v1.py
+++ dipl_Haupt_Vererbt_v1.py
@@ -260,10 +260,10 @@ class Frm_main(QMainWindow, Ui_StartWindow):
             insert_messwerte1 = """
             INSERT INTO Messwerte1 (Probe, Lichtstärke)
             VALUES 
-            ("P1", %s, "lum"),
-            ("P2", %s, "lum"),
-            ("P3", %s, "lum"),
-            ("P4", %s, "lum"),
+            ("P1", %s "lum"),
+            ("P2", %s "lum"),
+            ("P3", %s "lum"),
+            ("P4", %s "lum"),
             """
             self.cursor_mess1.execute(insert_messwerte1, (self.frm_kont.p1, self.frm_kont.p2, self.frm_kont.p3, self.frm_kont.p4))
 
@@ -271,10 +271,10 @@ class Frm_main(QMainWindow, Ui_StartWindow):
             insert_messwerte2 = """
             INSERT INTO Messwerte2 (Probe, Lichtstärke)
             VALUES 
-            ("P5", %s, "lum"),
-            ("P6", %s, "lum"),
-            ("P7", %s, "lum"),
-            ("P8", %s, "lum"),
+            ("P5", %s "lum"),
+            ("P6", %s "lum"),
+            ("P7", %s "lum"),
+            ("P8", %s "lum"),
             """
             self.cursor_mess2.execute(insert_messwerte2, (self.frm_kont.p5, self.frm_kont.p6, self.frm_kont.p7, self.frm_kont.p8))
 
