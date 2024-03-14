a547f7e76aee7b4356470950f3af14646196aeef
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-03-14 09:48:02 +0100

    null

--- dipl_I2C/i2c_detect_130324.py
+++ dipl_I2C/i2c_detect_130324.py
@@ -7,7 +7,7 @@ temp_address = 0x12  # Beispieladresse, ersetzen Sie sie durch die tatsächliche
 # Öffnen Sie den I2C-Bus (abhängig von Ihrem System kann die Busnummer variieren)
 bus = smbus.SMBus(7)  # Bus 1 öffnen, Sie können die richtige Busnummer je nach Ihrem Setup anpassen
 
-
+data_sent = False
 # Funktion zum Lesen von Daten vom Arduino
 def read_data():
     data = []
@@ -28,25 +28,40 @@ def read_data():
 
     return data
 
-# Daten vom Arduino lesen
-data_received = read_data()
+def read_detect_null():
+    # Nur Daten vom Slave lesen, wenn der Leseprozess nicht gestoppt wurde
+   
+    if data is None:             
+        data = read_detect_null()
 
-temp_d = data_received[0] 
-temp_a = data_received[1] 
-temp_e = data_received[2] 
-temp_de = data_received[3] 
-temp_an = data_received[4] 
-temp_el = data_received[5] 
-temp_den = data_received[6] 
-temp_ane = data_received[7] 
+    if data == 7:
+        data = read_detect_null()
+                        
+    if data == 0:
+        data = read_detect_null()
+    
+    if data == 5 and not data_sent:
+        data_sent = True
 
-# Die erhaltenen Daten anzeigen
-print("Temperaturen")
-print("Temperature (Denat):", temp_d)
-print("Temperature (Aneal):", temp_a)
-print("Temperature (Elong):", temp_e)
-print("Temperature (Denat):", temp_de)
-print("Temperature (Aneal):", temp_an)
-print("Temperature (Elong):", temp_el)
-print("Temperature (Denat):", temp_den)
-print("Temperature (Aneal):", temp_ane)
+        # Daten vom Arduino lesen
+        data_received = read_data()
+
+        temp_d = data_received[0] 
+        temp_a = data_received[1] 
+        temp_e = data_received[2] 
+        temp_de = data_received[3] 
+        temp_an = data_received[4] 
+        temp_el = data_received[5] 
+        temp_den = data_received[6] 
+        temp_ane = data_received[7] 
+
+        # Die erhaltenen Daten anzeigen
+        print("Temperaturen")
+        print("Temperature (Denat):", temp_d)
+        print("Temperature (Aneal):", temp_a)
+        print("Temperature (Elong):", temp_e)
+        print("Temperature (Denat):", temp_de)
+        print("Temperature (Aneal):", temp_an)
+        print("Temperature (Elong):", temp_el)
+        print("Temperature (Denat):", temp_den)
+        print("Temperature (Aneal):", temp_ane)
