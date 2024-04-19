6f82fad1eaaca813911fff21ed787d91fca391e4
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-04-19 13:47:49 +0200

    text neu

--- dipl_Haupt_v5.py
+++ dipl_Haupt_v5.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QTableWidget
 from PySide6.QtCore import QTimer, Signal
+from PySide6.QtGui import QFont
 import pymysql
 import smbus
 import time
@@ -117,7 +118,7 @@ class Frm_main(QMainWindow, Ui_StartWindow):
             self.frm_ergeb.tbl_dl.setColumnCount(2)  # Zwei Spalten
             self.frm_ergeb.tbl_dl.setHorizontalHeaderLabels(["Kategorie", "Anzahl"])
 
-            self. i =1
+            self.i =1
         
     def erlaubteTemp(self):
         self.frm_voraus.showFullScreen()
@@ -237,9 +238,12 @@ class Frm_main(QMainWindow, Ui_StartWindow):
         self.frm_tempanspruch.wasser_aneal.display(self.frm_tempanspruch.temp_aneal_kont)
         self.frm_tempanspruch.wasser_elong.display(self.frm_tempanspruch.temp_elong_kont)
 
-        self.temperaturen = [self.frm_tempDef.value_denat, self.frm_tempDef.value_aneal, self.frm_tempDef.value_elong]
+        self.temperaturen = self.frm_tempDef.value_denat, self.frm_tempDef.value_aneal, self.frm_tempDef.value_elong
 
         self.frm_tempanspruch.lbl_tempdef.setText(str(self.temperaturen))
+        font = QFont()
+        font.setPointSize(16)  # Hier die gewünschte Schriftgröße einstellen
+        self.frm_tempanspruch.lbl_tempdef.setFont(font)
 
         self.frm_tempanspruch.showFullScreen()
 
--- turtle_animation.py
+++ turtle_animation.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtGui import QFont
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,11 +9,20 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Werte anzeigen")
         self.setGeometry(100, 100, 400, 200)
 
-        label = QLabel("Hier sind einige Werte:", self)
-        label.setGeometry(50, 50, 200, 30)
+        self.label = QLabel("Hier sind einige Werte:", self)
+        self.label.setGeometry(50, 50, 200, 30)
 
-        value_label = QLabel("123", self)  # Beispielwert
-        value_label.setGeometry(250, 50, 100, 30)
+        self.value_label = QLabel("", self)  
+        self.value_label.setGeometry(250, 50, 100, 30)
+
+        self.value = 123
+        self.update_value_label()
+
+    def update_value_label(self):
+        self.value_label.setText(str(self.value))
+        font = QFont()
+        font.setPointSize(16)  # Hier die gewünschte Schriftgröße einstellen
+        self.value_label.setFont(font)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
