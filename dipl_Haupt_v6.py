3df1ef5915d055f2dc57dfc87593f0bf89af7f6c
Diplomarbeit-PCR <diplomarbeit.pcr@gmail.com>
2024-04-24 17:09:56 +0200

    ds

--- dipl_Haupt_v6.py
+++ dipl_Haupt_v6.py
@@ -554,7 +554,7 @@ class Frm_main(QMainWindow, Ui_StartWindow):
         data = []
         #i = 0
         self.bus.write_byte(self.detect_address, 10)
-        time.sleep(12)
+        time.sleep(15)
 
         # for i in range(8):
         #     i = i+1
